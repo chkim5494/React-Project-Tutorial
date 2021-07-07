5c4165d6c207ede581fee0f722a88749fb6536cb
kim chan hyeong <chkim5494@gmail.com>
2021-07-07 17:22:03 +0700

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
 import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+import { render } from '@testing-library/react';
+
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '김찬형',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '코기',
+    'birthday': '777777',
+    'gender': '남자',
+    'job': '하하하하'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '코카',
+    'birthday': '888888',
+    'gender': '여자',
+    'job': '강아지'
+  }
+]
 
 function App() {
+
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop management system!</h2>
+    <div>
+     {
+       customers.map(c => {
+         return (
+         <Customer
+         key={c.id}
+         id={c.id}
+         image={c.image}
+         name={c.name}
+         birthday={c.birthday}
+         gender={c.gender}
+         job={c.job}
+         />
+         );
+       })
+     }
     </div>
   );
 }
+
 
 export default App;
